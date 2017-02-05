3523f3b1f61f24065e1ee68b220bcdd0372fe185
Extreme_Piash <piash599@gmail.com>
2017-02-05 15:31:51 +0600

    added index page with authencation check

--- Python/urls.py
+++ Python/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from posts.views import post_home
-
+from posts.views import post_create, post_detail, post_list, post_update, post_delete
 urlpatterns = [
+    url(r'^create/$', post_create),
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/$', post_home),
+    url(r'^detail/$', post_detail),
+    url(r'^list/$', post_list),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def post_home(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def post_create(request):
+     if request.user.is_authenticated():
+     	context = {
+    		"title": "My User List"
+     	}
+     else:
+        context = {
+    		"title": "List"
+        }
+     return render(request, "index.html", context)
+
+def post_detail(request): #retrieve
+    return HttpResponse("<h1>detail</h1>")
+
+def post_list(request):#list items
+
+    return HttpResponse("<h1>list</h1>")
+
+def post_update(request):
+    return HttpResponse("<h1>update</h1>")
+
+def post_delete(request):
+    return HttpResponse("<h1>delete</h1>")
