43e5b8be80fc8672667d4977b4974b4e520ef402
Extreme_Piash <piash599@gmail.com>
2017-02-05 18:58:16 +0600

    added html with queryset

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
 def post_create(request):
@@ -17,8 +18,12 @@ def post_detail(request): #retrieve
     return HttpResponse("<h1>detail</h1>")
 
 def post_list(request):#list items
-
-    return HttpResponse("<h1>list</h1>")
+    queryset = Post.objects.all()
+    context = {
+        "object_list" : queryset,
+        "title": "List"
+    }
+    return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>update</h1>")
